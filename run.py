585f9f4584e0c15fc751295846dedd4479308732
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2026-03-11 22:46:35 +0800

    updated port

--- run.py
+++ run.py
@@ -8,4 +8,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv("FLASK_DEBUG") == "True")
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=os.getenv("FLASK_DEBUG") == "True", host="0.0.0.0", port=port)
